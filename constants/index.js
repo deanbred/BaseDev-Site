1513acfbe5e88bb67e047018d945dde2fe5096fd
deanbred <deanbred@gmail.com>
2023-08-06 00:13:06 -0400

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,9 @@
+const abi = require("./abi.json")
+const abiToken = require("./abiToken.json")
+const contractAddresses = require("./contractAddresses.json")
+
+module.exports = {
+  abi,
+  abiToken,
+  contractAddresses,
+}
---
+++ out/_next/static/GBNN6BggdJgrERUqJpspz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/343-5387878c6aabdea7.js","static/chunks/pages/index-fa05253cf462a51c.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
