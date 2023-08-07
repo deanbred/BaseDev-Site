8ce46d5b9f1f33dab460215cf72d1e7f69949467
deanbred <deanbred@gmail.com>
2023-08-06 21:51:21 -0400

    pics

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <nav className="p-3 z-10 flex flex-row justify-center sticky top-0 bg-gradient-to-r from-[#7ea6b9] to-[#376578] border-b-2 overflow-hidden">
       <div className="mr-auto">
-        <Image src="logo-00.jpg" width={72} height={72} alt="Logo"></Image>
+        <Image src="BaseDev.png" width={128} height={128} alt="Logo"></Image>
       </div>
       <div className=" ml-2 flex flex-col justify-center text-[#af292c] font-kake mr-auto lg:text-5xl md:text-4xl sm:text-xl font-semibold">
         BaseDev Token
