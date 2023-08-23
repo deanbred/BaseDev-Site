025bd1697dc88a6b805100a06e3f19d28c074575
deanbred <deanbred@gmail.com>
2023-08-23 05:33:36 -0400

    onlyfrens

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,12 @@ import Image from "next/image"
 
 export default function Header() {
   return (
-    <nav className="z-10 flex flex-row justify-center sticky top-0 bg-gradient-to-r from-slate-400 to-[#00010f] border-b-2 overflow-hidden">
-      <div className="mr-auto">
-        <Image src="logohex.png" width={72} height={72} alt="Logo"></Image>
+    <nav className="z-10 flex flex-row justify-center sticky top-0 bg-gradient-to-r from-slate-200 to-slate-500 border-b-2 overflow-hidden">
+      <div className="p-2 mr-auto">
+        <Image src="logo_large.png" width={72} height={72} alt="Logo"></Image>
       </div>
-      <div className="ml-2 flex flex-col justify-center text-[#0af58e] mr-auto lg:text-5xl md:text-4xl sm:text-xl font-semibold">
-        BaseDev Token
+      <div className="ml-2 flex flex-col justify-center font-kake text-[#b16541] mr-auto lg:text-5xl md:text-4xl sm:text-xl font-semibold">
+        OnlyFrens.tech
       </div>
       <div className="flex flex-col justify-center">
         <ConnectButton />
--- pages/index.js
+++ pages/index.js
@@ -10,38 +10,37 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>BaseDev Token</title>
+        <title>OnlyFrens.tech</title>
         <meta name="description" content="Crypto Token Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/logo128.png" />
+        <link rel="icon" href="/logo_large.png" />
       </Head>
       <Header />
-      <div className="flex min-h-screen flex-col overflow-hidden bg-no-repeat bg-cover bg-center bg-[url('../public/logo_bg.png')]">
+      <div className="flex min-h-screen flex-col overflow-hidden bg-no-repeat bg-contain bg-[url('../public/bg.jpg')]">
         <div className="p-4 lg:mr-auto md:mr-auto sm:mr-auto ">
           <button
-            className="p-2 font-semibold bg-[#0af58e] hover:text-white text-[#00010f] rounded-2xl"
-            onClick={async () => window.open("https://app.rocketswap.cc/exchange/swap")}
+            className="p-2 font-kake bg-[#69974f] hover:text-[#b16541] text-[#234afd] rounded-2xl"
+            onClick={async () =>
+              window.open(
+                "https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x1c823F81e0230cd8762c63e24D953735E0F8d67c"
+              )
+            }
           >
-            Buy on RocketSwap
+            Buy on UniSwap
           </button>
         </div>
         <div className="flex flex-col items-center">
-          <div className="p-3 text-center max-w-4xl bg-stone-300 opacity-90 md:text-xl sm:text-base rounded-2xl">
-            <p className="">
-              <a
-                href="https://dexscreener.com/base"
-                className="text-sky-800 hover:text-[#0af58e] font-semibold leading-7"
-              >
-                BaseDev DEX
-              </a>{" "}
-            </p>
-            <p>New token on the base blockchain</p>
-            <p>Updates Coming Soon!</p>
+          <div className="mb-64 flex flex-col items-center">
+            <audio id="music" controls autoPlay>
+              <source src="lemon_song.mp3" type="audio/mp3" />
+            </audio>
+          </div>
+          <div className="p-1 mt-64 font-kake text-center max-w-4xl bg-transparent md:text-2xl sm:text-base rounded-2xl">
             <p className="mt-1">
               Contract:{" "}
               <a
                 href="https://basescan.org/address/0x1c823f81e0230cd8762c63e24d953735e0f8d67c"
-                className="text-sky-800 hover:text-[#0af58e] font-semibold"
+                className="text-[#234afd] hover:text-[#b16541]"
               >
                 0x1c823F81e0230cd8762c63e24D953735E0F8d67c
               </a>{" "}
@@ -49,7 +48,7 @@ export default function Home() {
           </div>
           <div className="m-3 flex flex-row items-center justify-center space-x-4">
             <SocialIcon
-              url="https://twitter.com/basedev777"
+              url="https://twitter.com/Only_Frens_"
               network="twitter"
               fgColor="white"
             />
@@ -58,18 +57,42 @@ export default function Home() {
               network="telegram"
               fgColor="white"
             />
-            <SocialIcon url="" network="discord" fgColor="white" />
           </div>
-          <div className="p-3 mt-1 mb-3 max-w-3xl bg-stone-300 opacity-90 md:text-base sm:text-base rounded-2xl">
-            <a className="" data-theme="light" href="https://twitter.com/">
-              Tweets by @
-              <span className="text-sky-800 hover:text-[#0af58e] font-semibold">basedev777</span>
-            </a>
-            <script
-              async
-              src="https://platform.twitter.com/widgets.js"
-            ></script>
-          </div>
+        </div>
+
+        <div className="flex flex-wrap items-center justify-between">
+          <Image src="meme00.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme01.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme02.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme03.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme04.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme05.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme06.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme07.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme08.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme09.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme10.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme11.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme12.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme13.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme14.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme15.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme16.jpg" width={400} height={400} alt="meme" />
+          <Image src="meme17.jpg" width={400} height={400} alt="meme" />
+        </div>
+        <div className="p-3 m-6 text-center max-w-3xl font-kake bg-stone-200 opacity-90 md:text-base sm:text-base rounded-2xl">
+          <a
+            className=""
+            data-theme="light"
+            href="https://twitter.com/Only_Frens_"
+          >
+            Tweets by
+            <span className="text-[#234afd] hover:text-[#b16541]">
+              {" "}
+              @Only_Frens_
+            </span>
+          </a>
+          <script async src="https://platform.twitter.com/widgets.js"></script>
         </div>
       </div>
     </>
