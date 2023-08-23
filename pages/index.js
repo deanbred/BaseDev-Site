03845f89e78449ba44e8047430f58f1410a1cc6d
deanbred <deanbred@gmail.com>
2023-08-23 08:11:06 -0400

    pics

--- pages/index.js
+++ pages/index.js
@@ -32,13 +32,19 @@ export default function Home() {
         <div className="flex flex-col items-center">
           <div className="mt-96 font-kake text-center max-w-4xl bg-transparent md:text-2xl sm:text-base rounded-2xl">
             <p className="text-slate-900 hover:text-red-700">
-              <a href="https://etherscan.io" className="">
+              <a
+                href="https://etherscan.io/token/0x52e8336322c8fd0f99000e80f8c5eb0acd1c9bc3"
+                className=""
+              >
                 {" "}
                 Contract
               </a>{" "}
             </p>
             <p className="text-slate-900 hover:text-green-700">
-              <a href="https://etherscan.io" className="">
+              <a
+                href="https://dexscreener.com/ethereum/0x52e8336322C8fD0f99000e80F8C5EB0acD1C9Bc3"
+                className=""
+              >
                 Chart
               </a>{" "}
             </p>
@@ -57,10 +63,10 @@ export default function Home() {
           </div>
         </div>
         <div className="flex flex-col items-center">
-            <audio id="music" controls autoPlay>
-              <source src="lemon_song.mp3" type="audio/mp3" />
-            </audio>
-          </div>
+          <audio id="music" controls autoPlay>
+            <source src="lemon_song.mp3" type="audio/mp3" />
+          </audio>
+        </div>
 
         <div className="flex flex-wrap items-center justify-between">
           <Image src="meme00.jpg" width={400} height={400} alt="meme" />
